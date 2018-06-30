aa865acc450c0db5b3d0aaeba19bf309bd2e6021
Raffaelbrx <raffaele56t@gmail.com>
2018-06-30 16:51:44 +0200

    Inizializzato doublyLinkedList

---
+++ doublyLinkedList/liste.h
@@ -0,0 +1,35 @@
+/*
+Raffaele Sarracino
+*/
+#ifndef LISTE_LIB_H
+#define LISTE_LIB_H
+
+struct nodo{
+    int data;
+    struct nodo *next;
+    struct nodo *prev;
+};
+
+//verifica se la lista è vuota oppure no, usata per semplicità
+int isEmpty(struct nodo *);
+//Crea una lista costituita unicamente dal valore sentinella
+struct nodo *creaLista();
+//Inserisce un elemento in testa alla lista
+void insInTesta(struct nodo *,int );
+//Inserisce un elemento in coda alla lista o in testa se la lista è vuota
+void insInCoda(struct nodo *,int);
+//inserisce un elemento in ordine oppure in testa se la lista è vuota
+void insInOrdine(struct nodo *,int);
+//Stampa una lista circolare
+void stampaLista(struct nodo *);
+//canella l'intera lista tranne l'elemento sentinella
+void cancellaLista(struct nodo *);
+//cancella un elemento e le sue ripetiozioni
+void cancellaElemento(struct nodo*,int);
+//ordina una lista in ordine crescente
+void ordinaCrescente(struct nodo*);
+//ordina una lista in ordine crescente
+void ordinaDecrescente(struct nodo*);
+//cerca un elemento nella lista
+int cercaElemento(struct nodo *,int);
+#endif
---
+++ doublyLinkedList/main.c
@@ -0,0 +1,115 @@
+/*
+Raffaele Sarracino
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "liste.h"
+
+void stampaMenu();
+
+int main(){
+    char op = 'x';
+    int dim = 0,i = 0,x = 0;
+    struct nodo *lista = creaLista();
+
+    while(op != 'z'){
+        stampaMenu();
+        printf("\tScegli l'operazione da eseguire: ");
+        scanf("\n%c",&op);
+
+        switch(op){
+            case 'a':{
+                printf("Inserisci il numero di elementi da inserire: ");
+                scanf("%d",&dim);
+
+                for(i=0;i<dim;i++){
+                    scanf("%d",&x);
+                    insInTesta(lista,x);
+                }
+
+                break;
+            }
+            case 'b':{
+                printf("Inserisci il numero di elementi da inserire: ");
+                scanf("%d",&dim);
+
+                for(i=0;i<dim;i++){
+                    scanf("%d",&x);
+                    insInCoda(lista,x);
+                }
+
+                break;
+            }
+            case 'c':{
+                printf("Inserisici il numero di elementi da inserire: ");
+                scanf("%d",&dim);
+                for(i=0;i<dim;i++){
+                    scanf("%d",&x);
+                    insInOrdine(lista,x);
+                }
+                break;
+            }
+            case 'd':{
+                printf("Ordino la lista in ordine crescente...\n");
+                ordinaCrescente(lista);
+                stampaLista(lista);
+                printf("\n");
+                break;
+            }
+            case 'e':{
+                printf("Ordino la lista in ordine decrescente...\n");
+                ordinaDecrescente(lista);
+                stampaLista(lista);
+                printf("\n");
+                break;
+            }
+            case 'f':{
+                stampaLista(lista);
+                break;
+            }
+            case 'g':{
+                printf("Inserisci l'elemento da eliminare: ");
+                scanf("%d",&x);
+                cancellaElemento(lista,x);
+                break;
+            }
+            case 'h':{
+                printf("Inserisci l'elemento da cercare: ");
+                scanf("%d",&x);
+
+                if(cercaElemento(lista,x)){
+                    printf("\nL'elemento cercato e' presente nella lista;\n");
+                }
+                else{
+                    printf("\nL'elemento cercato non e' presente nella lista;\n");
+                }
+                break;
+            }
+            case 'i':{
+                printf("Cancello la lista ...\n");
+                cancellaLista(lista);
+                if(isEmpty(lista)) printf("\nLista cancellata correttamente\n");
+                else printf("\nErrore durante la cancellazione;\n");
+                break;
+            }
+        }
+    }
+    cancellaLista(lista);
+    free(lista);
+    return 0;
+}
+
+void stampaMenu(){
+    printf("\t===== MENU =====\n");
+    printf("\ta)Inserisci elementi in testa;\n");
+    printf("\tb)Inserisci elementi in coda;\n");
+    printf("\tc)Inserisci elementi in ordine;\n");
+    printf("\td)Ordina la lista in ordine crescente;\n");
+    printf("\te)Ordina la funzione in ordine decrescente;\n");
+    printf("\tf)Stampa lista;\n");
+    printf("\tg)Elimina elemento;\n");
+    printf("\th)Cerca un elemento;\n");
+    printf("\ti)Elimina l'intera lista;\n");
+    printf("\tz)Termina programma;\n");
+}
