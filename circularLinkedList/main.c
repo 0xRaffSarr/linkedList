8e7103f4e5184339244c2d3558e3ab08b411f0b8
Raffaelbrx <raffaele56t@gmail.com>
2018-08-01 23:45:37 +0200

    corretto menu

--- circularLinkedList/main.c
+++ circularLinkedList/main.c
@@ -106,7 +106,7 @@ void stampaMenu(){
     printf("\tb)Inserisci elementi in coda;\n");
     printf("\tc)Inserisci elementi in ordine;\n");
     printf("\td)Ordina la lista in ordine crescente;\n");
-    printf("\te)Ordina la funzione in ordine decrescente;\n");
+    printf("\te)Ordina la lista in ordine decrescente;\n");
     printf("\tf)Stampa lista;\n");
     printf("\tg)Elimina elemento;\n");
     printf("\th)Cerca un elemento;\n");
